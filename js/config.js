450d2a08ca054217f4d4bc2698cfaccf1ff72e2d
Nuru.N <68421564+Nuru20@users.noreply.github.com>
2026-06-25 14:59:39 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://oscetjtbgnhtrswbkvgi.supabase.co/rest/v1/';
+const SUPABASE_KEY = 'sb_publishable_MCB0SxHcBGdvNxuWwAkwUQ_A4gTa4CH';
