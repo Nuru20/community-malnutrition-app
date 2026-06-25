26c26cf82894b1591e8c32b7d02ed2c4aca950ef
NURU. N <nuru@poblenou-138-101.eduroamcp.upf.edu>
2026-06-25 15:28:02 +0200

    Fix Supabase URL and variable scope

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = 'https://oscetjtbgnhtrswbkvgi.supabase.co/rest/v1/';
-const SUPABASE_KEY = 'sb_publishable_MCB0SxHcBGdvNxuWwAkwUQ_A4gTa4CH';
+var SUPABASE_URL = 'https://oscetjtbgnhtrswbkvgi.supabase.co';
+var SUPABASE_KEY = 'sb_publishable_MCB0SxHcBGdvNxuWwAkwUQ_A4gTa4CH';
